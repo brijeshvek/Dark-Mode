cc9175597bfb579cb32a42c890005b206fd22514
Brijesh Vekariya <125974636+brijesh2811@users.noreply.github.com>
2024-06-13 10:15:57 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css'; 
+import DarkMode from './components/DarkMode';
+// import FormText from './components/FormText';
+
+function App() {
+  return(
+    <>
+    <div className="container">
+      <DarkMode/>
+    </div>
+    </>
+  );
+}
+
+export default App;
